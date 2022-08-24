fa3471ec761e43389da3f26416d971ad45da01f5
jfgen <jfgencarnacao@gmail.com>
2022-08-24 15:52:21 +0100

    Up to lesson 140, new Blog app

---
+++ blog/src/screens/IndexScreen.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Button, FlatList, Text, View } from "react-native";
+import { Context } from "../context/BlogContext";
+
+const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
+
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(post) => post.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+export default IndexScreen;
